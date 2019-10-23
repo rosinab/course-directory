530ef2b32cd222cf1f63c10ff3c8572b236f1d95
Rosina <rosinabrooker@yahoo.co.uk>
2019-10-23 13:06:56 +0100

    Updates

---
+++ precache-manifest.a7fdcd07637e7d9c4b93e2e05e561099.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "a93889bae2395888f0d1",
+    "url": "/course-directory/static/js/main.a93889ba.chunk.js"
+  },
+  {
+    "revision": "f5ae2494a8dbd9f95f4b",
+    "url": "/course-directory/static/js/1.f5ae2494.chunk.js"
+  },
+  {
+    "revision": "a93889bae2395888f0d1",
+    "url": "/course-directory/static/css/main.8aa41495.chunk.css"
+  },
+  {
+    "revision": "e1bdf3b8156e475afb804b235fb55ee1",
+    "url": "/course-directory/index.html"
+  }
+];
